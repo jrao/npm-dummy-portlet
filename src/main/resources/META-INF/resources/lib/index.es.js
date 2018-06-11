9ab3a8dd55d53e94e4eac843684660b94c68ed67
Jesse Rao <jesse.rao@liferay.com>
2018-06-11 17:32:54 -0400

    Fix JS

--- src/main/resources/META-INF/resources/lib/index.es.js
+++ src/main/resources/META-INF/resources/lib/index.es.js
@@ -1,3 +1,6 @@
-export default function() {
-	out.innerHTML += '<p>Hello from NPM Dummy Portlet!</p>';
+export default function(params) {
+    var out = document.getElementById(params.portletElementId);
+
+    out.innerHTML = 'Porlet Namespace: ' + params.portletNamespace +
+            "<br/>" + 'Context Path: ' + params.contextPath + "<br/>" + 'Portlet Element Id: ' + params.portletElementId;
 }
