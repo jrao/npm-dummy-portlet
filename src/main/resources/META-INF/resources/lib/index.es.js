eaf2a64b3b82c9176b18c3ab55661ec4f846fce1
Jesse Rao <jesse.rao@liferay.com>
2018-06-18 16:51:29 -0400

    Clean JS

--- src/main/resources/META-INF/resources/lib/index.es.js
+++ src/main/resources/META-INF/resources/lib/index.es.js
@@ -1,5 +1,12 @@
+import isArray from 'isarray';
+
 export default function(params) {
     var out = document.getElementById(params.portletElementId);
+    
+    var t = isArray([]);
 
-    out.innerHTML = 'Portlet Namespace: ' + params.portletNamespace + '<br/>Context Path: ' + params.contextPath + '<br/>Portlet Element Id: ' + params.portletElementId;
+    out.innerHTML = 'Portlet Namespace: ' + params.portletNamespace +
+    '<br/>Context Path: ' + params.contextPath +
+    '<br/>Portlet Element Id: ' + params.portletElementId +
+    '<br/>Calling isArray([]) returns ' + t;
 }
