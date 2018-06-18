e2de74053720241d9920bca14d4dd55d12daf42d
Jesse Rao <jesse.rao@liferay.com>
2018-06-18 11:41:37 -0400

    Fix typo

--- src/main/resources/META-INF/resources/lib/index.es.js
+++ src/main/resources/META-INF/resources/lib/index.es.js
@@ -1,6 +1,5 @@
 export default function(params) {
     var out = document.getElementById(params.portletElementId);
 
-    out.innerHTML = 'Porlet Namespace: ' + params.portletNamespace +
-            "<br/>" + 'Context Path: ' + params.contextPath + "<br/>" + 'Portlet Element Id: ' + params.portletElementId;
+    out.innerHTML = 'Portlet Namespace: ' + params.portletNamespace + '<br/>Context Path: ' + params.contextPath + '<br/>Portlet Element Id: ' + params.portletElementId;
 }
